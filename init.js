90112bb5be842a2c612bf32293b4a2a6b811abbe
Tomakiston <tomasmeme86@gmail.com>
2025-09-16 16:00:37 -0300

    teste classe Overworld

---
+++ init.js
@@ -0,0 +1,8 @@
+(function () {
+    //console.log("ta funcionando")
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+    overworld.init();
+
+})();
