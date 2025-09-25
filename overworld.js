a4317ae9a268a53f1b3ff70dfbb35f50d4dcd092
Tomakiston <tomasmeme86@gmail.com>
2025-09-25 15:55:02 -0300

    Objeto de Classe

--- overworld.js
+++ overworld.js
@@ -5,7 +5,28 @@ class Overworld {
         this.ctx = this.canvas.getContext("2d");
     }
 
-    init(){
+    init() {
         console.log("Hello from the overworld!", this);
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image, 0, 0);
+        };
+        image.src = "/images/maps/DemoLower.png";
+
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+        });
+
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/images/characters/people/npc1.png"
+        });
+
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200);
     }
 }
