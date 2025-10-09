dabd5d39ddcde9d4fee9efc45493a4c74d2f0eb9
Tomakiston <tomasmeme86@gmail.com>
2025-10-09 15:55:53 -0300

    Animação Básica

--- overworld.js
+++ overworld.js
@@ -3,30 +3,35 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => {
+            this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+            this.map.drawLowerImage(this.ctx);
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.update({
+                    arrow: this.directionInput.direction
+                })
+                object.sprite.draw(this.ctx);
+            })
+
+            this.map.drawUpperImage(this.ctx);
+
+            requestAnimationFrame(() => {
+                step();
+            })
+        }
+        
+        step();
     }
 
     init() {
         console.log("Hello from the overworld!", this);
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0);
-        };
-        image.src = "/images/maps/DemoLower.png";
-
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        });
-
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "/images/characters/people/npc1.png"
-        });
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200);
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+        this.directionInput = new DirectionInput();
+        this.directionInput.init();
+        this.startGameLoop();
     }
 }
