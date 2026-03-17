ee4ad4a9e1b8326a3991d9815621e7697a8f55c4
Tomakiston <tomasmeme86@gmail.com>
2026-03-17 15:48:44 -0300

    Menu de Pause Final

--- KeyboardMenu.js
+++ KeyboardMenu.js
@@ -79,8 +79,8 @@ class KeyboardMenu {
         })
         this.down = new KeyPressListener("ArrowDown", () => {
             const current = Number(this.prevFocus.getAttribute("data-button"));
-            const nextButton = Array.from(this.element.querySelectorAll("button[data-button]")).reverse().find(el => {
-                return el.dataset.button < current && !el.disabled;
+            const nextButton = Array.from(this.element.querySelectorAll("button[data-button]")).find(el => {
+                return el.dataset.button > current && !el.disabled;
             })
 
             nextButton?.focus();
