abd7146fb463960ef54418e9b17b2cf7df65a3d2
Tomakiston <tomasmeme86@gmail.com>
2026-04-14 15:56:14 -0300

    Npcs Novos

--- OverworldMap.js
+++ OverworldMap.js
@@ -117,12 +117,14 @@ window.OverworldMaps = {
         lowerSrc: "images/maps/DemoLower.png",
         upperSrc: "images/maps/DemoUpper.png",
         gameObjects: {
-            hero: new Person({
+            hero: {
+                type: "Person",
                 isPlayerControlled: true,
                 x: utils.withGrid(5),
                 y: utils.withGrid(6)
-            }),
-            npcA: new Person({
+            },
+            npcA: {
+                type: "Person",
                 x: utils.withGrid(7),
                 y: utils.withGrid(9),
                 src: "images/characters/people/npc1.png",
@@ -150,8 +152,25 @@ window.OverworldMaps = {
                         ]
                     }
                 ]
-            }),
-            npcB: new Person({
+            },
+            npcC: {
+                type: "Person",
+                x: utils.withGrid(4),
+                y: utils.withGrid(8),
+                src: "images/characters/people/npc1.png",
+                behaviorLoop: [
+                    {type: "stand", direction: "left", time: 500},
+                    {type: "stand", direction: "down", time: 500},
+                    {type: "stand", direction: "right", time: 500},
+                    {type: "stand", direction: "up", time: 500},
+                    {type: "walk", direction: "left"},
+                    {type: "walk", direction: "down"},
+                    {type: "walk", direction: "right"},
+                    {type: "walk", direction: "up"}
+                ]
+            },
+            npcB: {
+                type: "Person",
                 x: utils.withGrid(8),
                 y: utils.withGrid(5),
                 src: "images/characters/people/erio.png",
@@ -171,13 +190,14 @@ window.OverworldMaps = {
                     ]
                 }
                ]
-            }),
-            pizzaStone: new PizzaStone({
+            },
+            pizzaStone: {
+                type: "PizzaStone",
                 x: utils.withGrid(2),
                 y: utils.withGrid(7),
                 storyFlag: "USED_PIZZA_STONE",
                 pizzas: ["v001", "f001"]
-            })
+            }
         },
         walls: {
             [utils.asGridCoord(7,6)]: true,
@@ -201,7 +221,13 @@ window.OverworldMaps = {
             [utils.asGridCoord(5,10)]: [
                 {
                     events: [
-                        {type: "changeMap", map: "Kitchen"}
+                        {
+                            type: "changeMap",
+                            map: "Kitchen",
+                            x: utils.withGrid(2),
+                            y: utils.withGrid(2),
+                            direction: "down"
+                        }
                     ]
                 }
             ]
@@ -212,23 +238,51 @@ window.OverworldMaps = {
         lowerSrc: "images/maps/KitchenLower.png",
         upperSrc: "images/maps/KitchenUpper.png",
         gameObjects: {
-            hero: new Person({
+            hero: {
+                type: "Person",
                 isPlayerControlled: true,
-                x: utils.withGrid(5),
-                y: utils.withGrid(5)
-            }),
-            npcB: new Person({
                 x: utils.withGrid(10),
-                y: utils.withGrid(8),
+                y: utils.withGrid(5)
+            },
+            kitchenNpcA: {
+                type: "Person",
+                x: utils.withGrid(9),
+                y: utils.withGrid(5),
+                direction: "up",
+                src: "images/characters/people/npc8.png",
+                talking: [
+                    {
+                        events: [
+                            {type: "textMessage", text: "**Ele não quer falar com você**"}
+                        ]
+                    }
+                ]
+            },
+            kitchenNpcB: {
+                type: "Person",
+                x: utils.withGrid(3),
+                y: utils.withGrid(6),
                 src: "images/characters/people/npc3.png",
                 talking: [
                     {
                         events: [
-                            {type: "textMessage", text: "Você conseguiu!", faceHero: "npcB"}
+                            {type: "textMessage", text: "As pessoas aqui levam seus trabalhos muito a serio", faceHero: "kitchenNpcB"}
                         ]
                     }
+                ],
+                behaviorLoop: [
+                    {type: "walk", direction: "right"},
+                    {type: "walk", direction: "right"},
+                    {type: "walk", direction: "down"},
+                    {type: "walk", direction: "down"},
+                    {type: "walk", direction: "left"},
+                    {type: "walk", direction: "left"},
+                    {type: "walk", direction: "up"},
+                    {type: "walk", direction: "up"},
+                    {type: "stand", direction: "up", time: 500},
+                    {type: "stand", direction: "left", time: 500}
                 ]
-            }),
+            },
         },
         cutsceneSpaces: {
             [utils.asGridCoord(5,10)]: [
@@ -236,14 +290,74 @@ window.OverworldMaps = {
                     events: [
                         {
                             type: "changeMap",
-                            map: "Street",
-                            x: utils.withGrid(29),
-                            y: utils.withGrid(9),
+                            map: "DiningRoom",
+                            x: utils.withGrid(7),
+                            y: utils.withGrid(3),
                             direction: "down"
                         }
                     ]
                 }
+            ],
+            [utils.asGridCoord(10,6)]: [
+                {
+                    disqualify: ["SEEN_INTRO"],
+                    events: [
+                        {type: "addStoryFlag", flag: "SEEN_INTRO"},
+                        {type: "textMessage", text: "*No seu primeiro dia como pizzaiolo em um estabelecimento famoso da cidade, você está cortando ingredientes.*"},
+                        {type: "walk", who: "kitchenNpcA", direction: "down"},
+                        {type: "stand", who: "kitchenNpcA", direction: "right", time: 200},
+                        {type: "stand", who: "hero", direction: "left", time: 200},
+                        {type: "textMessage", text: "Hum. Esse é o seu melhor trabalho?"},
+                        {type: "textMessage", text: "Esses pepperonis estão completamente instáveis! Os formatos dos pimentões estão todos errados!"},
+                        {type: "textMessage", text: "Nem me fale dos cogumelos."},
+                        {type: "textMessage", text: "Você nunca vai se dar bem no ramo da pizzas!"},
+                        {type: "stand", who: "kitchenNpcA", direction: "right", time: 200},
+                        {type: "walk", who: "kitchenNpcA", direction: "up"},
+                        {type: "stand", who: "kitchenNpcA", direction: "up", time: 300},
+                        {type: "stand", who: "hero", direction: "down", time: 400},
+                        {type: "textMessage", text: "*A competição é acirrada! Você deve dedicar algum tempo a aprimorar sua equipe de pizzaiolos e suas habilidades.*"},
+                        {type: "changeMap", map: "Street", x: utils.withGrid(5), y: utils.withGrid(10), direction: "down"}
+                    ]
+                }
             ]
+        },
+        walls: {
+            [utils.asGridCoord(2,4)]: true,
+            [utils.asGridCoord(3,4)]: true,
+            [utils.asGridCoord(5,4)]: true,
+            [utils.asGridCoord(6,4)]: true,
+            [utils.asGridCoord(7,4)]: true,
+            [utils.asGridCoord(8,4)]: true,
+            [utils.asGridCoord(11,4)]: true,
+            [utils.asGridCoord(11,5)]: true,
+            [utils.asGridCoord(12,5)]: true,
+            [utils.asGridCoord(1,5)]: true,
+            [utils.asGridCoord(1,6)]: true,
+            [utils.asGridCoord(1,7)]: true,
+            [utils.asGridCoord(1,9)]: true,
+            [utils.asGridCoord(2,9)]: true,
+            [utils.asGridCoord(6,7)]: true,
+            [utils.asGridCoord(7,7)]: true,
+            [utils.asGridCoord(9,7)]: true,
+            [utils.asGridCoord(10,7)]: true,
+            [utils.asGridCoord(9,9)]: true,
+            [utils.asGridCoord(10,9)]: true,
+            [utils.asGridCoord(3,10)]: true,
+            [utils.asGridCoord(4,10)]: true,
+            [utils.asGridCoord(6,10)]: true,
+            [utils.asGridCoord(7,10)]: true,
+            [utils.asGridCoord(8,10)]: true,
+            [utils.asGridCoord(11,10)]: true,
+            [utils.asGridCoord(12,10)]: true,
+            [utils.asGridCoord(0,8)]: true,
+            [utils.asGridCoord(5,11)]: true,
+            [utils.asGridCoord(4,3)]: true,
+            [utils.asGridCoord(9,4)]: true,
+            [utils.asGridCoord(10,4)]: true,
+            [utils.asGridCoord(13,6)]: true,
+            [utils.asGridCoord(13,7)]: true,
+            [utils.asGridCoord(13,8)]: true,
+            [utils.asGridCoord(13,9)]: true
         }
     },
     Street: {
@@ -251,11 +365,29 @@ window.OverworldMaps = {
         lowerSrc: "images/maps/StreetLower.png",
         upperSrc: "images/maps/StreetUpper.png",
         gameObjects: {
-            hero: new Person({
+            hero: {
+                type: "Person",
                 isPlayerControlled: true,
                 x: utils.withGrid(30),
                 y: utils.withGrid(10)
-            })
+            },
+            streetNpcA: {
+                type: "Person",
+                x: utils.withGrid(9),
+                y: utils.withGrid(11),
+                src: "images/characters/people/npc2.png",
+                behaviorLoop: [
+                    {type: "stand", direction: "right", time: 1400},
+                    {type: "stand", direction: "up", time: 900}
+                ],
+                talking: [
+                    {
+                        events: [
+                            {type: "textMessage", text: "Todos os pizzaiolos ambiciosos se reúnem na Avenida Anchovas.", faceHero: "streetNpcA"}
+                        ]
+                    }
+                ]
+            }
         },
         cutsceneSpaces: {
             [utils.asGridCoord(29,9)]: [
